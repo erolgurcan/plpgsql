63b510877e707ed3d272fc8061eae091c423a4f2
Erol Gurcan <59280972+erolgurcan@users.noreply.github.com>
2022-09-07 08:12:31 -0700

    Create create_pay_table.sql

---
+++ create_pay_table.sql
@@ -0,0 +1,53 @@
+/*Creates the sql query below */ 
+ 
+create or replace function public.__update_paid_cross_table() returns void 
+language plpgsql
+as $function$
+declare 
+	rec record;
+	months text;
+	counter record;
+	cnt int;
+	final_str text;
+	len int;
+begin
+	cnt := 0;
+	months := '';
+	select count(distinct( month_year )) as cnt into counter from pay_table;
+	raise notice '%', counter.cnt;
+	len := counter.cnt;
+	for rec in select distinct month_year from pay_table order by 1 
+	loop 
+		cnt := cnt+ 1;
+		if months = '' and cnt < len  then months := concat('"', rec.month_year, '"', ' text', ',');
+		else  
+			if cnt = 7 then months := concat(  months , '
+' ,  '"', rec.month_year, '"' , ' text');
+			else months := concat(  months , '
+' ,  '"', rec.month_year, '"' , ' text' , ',');
+			end if;
+		end if;
+	end loop; 
+
+	raise notice '%' , final_str;
+end;
+$function$;
+
+
+
+
+select *  from crosstab( 
+   'select player_full_name, month_year, paid_amount from pay_table order by 1,2', 'select distinct month_year from pay_table order by 1 ' 
+)
+as(
+player_full_name text,
+"07_2022" text,
+"08_2022" text,
+"09_2022" text,
+"10_2022" text,
+"11_2022" text,
+"12_2022" text, 
+"01_2023" text .......
+)
+ 
+
